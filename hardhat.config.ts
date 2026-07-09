5db0a32b406c0ff37da0259dfdf7697e19286b64
icodejsx <kenzynweke@gmail.com>
2026-07-09 11:02:52 +0100

    done with the installation of this hardhat

---
+++ hardhat.config.ts
@@ -0,0 +1,23 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    fuji: {
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      chainId: 43113,
+      accounts: [], // we'll add your private key later
+    },
+  },
+};
+
+export default config;
